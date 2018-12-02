4421e539777e66622d98f0f8c4102d98220b1f76
Sachin Raj <sraj@microsoft.com>
2018-12-02 11:39:08 -0800

    Update Startup.cs

--- Source/Tailwind.Traders.Web/Startup.cs
+++ Source/Tailwind.Traders.Web/Startup.cs
@@ -18,6 +18,7 @@ namespace Tailwind.Traders.Web
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        //This needs some refactoring
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
