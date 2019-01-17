f23fe565f35975681b07ce5c844db74416f6a192
Ignacio Villanueva <ignaciodenuevo@gmail.com>
2019-01-17 17:44:01 +0100

    Added login layout

--- Source/Tailwind.Traders.Web/ClientApp/src/shared/header/header.js
+++ Source/Tailwind.Traders.Web/ClientApp/src/shared/header/header.js
@@ -1,24 +1,27 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-import { NamespacesConsumer } from "react-i18next";
+import { NamespacesConsumer } from 'react-i18next';
 
-import APIClient from "../../ApiClient";
-import UserPortrait from "./components/userPortrait";
+import APIClient from '../../ApiClient';
+// import UserPortrait from './components/userPortrait'; => this will be needed when the user is logged in
+import LoginModal from './components/loginModal';
 
-import { ReactComponent as Logo } from "../../assets/images/logo-horizontal.svg";
-import { ReactComponent as Hamburger } from "../../assets/images/icon-menu.svg";
-import { ReactComponent as Cart } from "../../assets/images/icon-cart.svg";
-import { ReactComponent as Search } from "../../assets/images/icon-search.svg";
-import { ReactComponent as Close } from "../../assets/images/icon-close.svg";
+import { ReactComponent as Logo } from '../../assets/images/logo-horizontal.svg';
+import { ReactComponent as Close } from '../../assets/images/icon-close.svg';
+import { ReactComponent as Hamburger } from '../../assets/images/icon-menu.svg';
+import { ReactComponent as Cart } from '../../assets/images/icon-cart.svg';
+import { ReactComponent as Search } from '../../assets/images/icon-search.svg';
 
 class Header extends Component {
     constructor() {
         super();
         this.state = {
             isopened: false,
+            isomodalpened: false,
             profile: {},
         };
+        this.loginModalRef = React.createRef();
     }
 
     async componentDidMount() {
@@ -27,10 +30,9 @@ class Header extends Component {
 
         const setComponentVisibility = this.setComponentVisibility.bind(this);
         setComponentVisibility(document.documentElement.clientWidth);
-        window.addEventListener("resize", function () {
+        window.addEventListener('resize', function() {
             setComponentVisibility(document.documentElement.clientWidth);
         });
-
     }
 
     setComponentVisibility(width) {
@@ -45,8 +47,12 @@ class Header extends Component {
         }));
     };
 
+    toggleModalClass = () => {
+        this.loginModalRef.current.toggleModalClass();
+    };
+
     render() {
-        const { profile } = this.state
+        // const { profile } = this.state; =>  this will be needed when the user is logged in
         return (
             <NamespacesConsumer>
                 {t => (
@@ -54,34 +60,34 @@ class Header extends Component {
                         <Link to="/">
                             <Logo />
                         </Link>
-                        <nav className={this.state.isopened ? "main-nav is-opened" : "main-nav"}>
+                        <nav className={this.state.isopened ? 'main-nav is-opened' : 'main-nav'}>
                             <Link className="main-nav__item" to="/list/homeappliances">
-                                {t("shared.header.homeAppliances")}
+                                {t('shared.header.homeAppliances')}
                             </Link>
                             <Link className="main-nav__item" to="/list/sink">
-                                {t("shared.header.sink")}
+                                {t('shared.header.sink')}
                             </Link>
                             <Link className="main-nav__item" to="/list/home">
-                                {t("shared.header.home")}
+                                {t('shared.header.home')}
                             </Link>
                             <Link className="main-nav__item" to="/list/gardening">
-                                {t("shared.header.gardening")}
+                                {t('shared.header.gardening')}
                             </Link>
                             <Link className="main-nav__item" to="/list/decor">
-                                {t("shared.header.decor")}
+                                {t('shared.header.decor')}
                             </Link>
                             <Link className="main-nav__item" to="/list/kitchen">
-                                {t("shared.header.kitchen")}
+                                {t('shared.header.kitchen')}
                             </Link>
                             <Link className="main-nav__item" to="/list/diytools">
-                                {t("shared.header.diytools")}
+                                {t('shared.header.diytools')}
                             </Link>
                             <div className="main-nav__actions">
                                 <Link className="main-nav__item" to="/profile">
-                                    {t("shared.header.profile")}
+                                    {t('shared.header.profile')}
                                 </Link>
                                 <button className="u-empty main-nav__item">
-                                    {t("shared.header.logout")}
+                                    {t('shared.header.logout')}
                                 </button>
                             </div>
                             <button className="u-empty js-close" onClick={this.toggleClass}>
@@ -90,15 +96,19 @@ class Header extends Component {
                         </nav>
                         <nav className="secondary-nav">
                             <Search />
-                            <UserPortrait {...profile} />
+                            {/* <UserPortrait {...profile} /> this will be needed when the user is logged in */}
+                            <div onClick={this.toggleModalClass}>Login</div>
                             <Link className="secondary-nav__cart" to="/shopping-cart">
                                 <Cart />
-                                <div className="secondary-nav__cart-number">{this.props.quantity}</div>
+                                <div className="secondary-nav__cart-number">
+                                    {this.props.quantity}
+                                </div>
                             </Link>
-                            <button className="u-empty js-open" onClick={this.toggleClass}>
+                            <button className="u-empty" onClick={this.toggleModClass}>
                                 <Hamburger />
                             </button>
                         </nav>
+                        <LoginModal ref={this.loginModalRef} />
                     </header>
                 )}
             </NamespacesConsumer>
