d4f81a5aa368d5dbf7d06780a33a9df73b3d8cad
Lily Motley <limotley@microsoft.com>
2019-09-12 14:07:56 -0700

    Added more context/action features

--- Source/Tailwind.Traders.Web/ClientApp/src/pages/home/homeContainer.js
+++ Source/Tailwind.Traders.Web/ClientApp/src/pages/home/homeContainer.js
@@ -8,6 +8,7 @@ import Powertools from "../../assets/images/home_powertools.jpg";
 import Plumbing from "../../assets/images/home_plumbing.jpg";
 import Garden from "../../assets/images/home_gardencenter.jpg";
 import Electrical from "../../assets/images/home_electrical.jpg";
+import recommended from "./components/recommended/recommended";
 
 class HomeContainer extends Component {
     constructor() {
@@ -18,26 +19,26 @@ class HomeContainer extends Component {
                     title: "Power Tools",
                     imageUrl: Powertools,
                     cssClass: "grid__item-a",
-                    url: "/list/diytools"
-                },
-                {
-                    title: "Plumbing",
-                    imageUrl: Plumbing,
-                    cssClass: "grid__item-b",
                     url: "/list/kitchen"
                 },
                 {
                     title: "Electrical",
                     imageUrl: Electrical,
-                    cssClass: "grid__item-c",
+                    cssClass: "grid__item-b",
                     url: "/list/home"
                 },
                 {
                     title: "Garden Center",
                     imageUrl: Garden,
-                    cssClass: "grid__item-d",
+                    cssClass: "grid__item-c",
                     url: "/list/gardening"
                 },
+                {
+                    title: "Plumbing",
+                    imageUrl: Plumbing,
+                    cssClass: "grid__item-d",
+                    url: "/list/diytools"
+                }
             ],
             popularProducts: [],
             loading: true,
@@ -45,15 +46,51 @@ class HomeContainer extends Component {
     }
 
     async componentDidMount() {
+
         if (this.props.userInfo.loggedIn) {
             await this.renderPopularProducts()
         }
+        this.getRank()
     }
 
     async shouldComponentUpdate(nextProps) {
         if ((this.props.userInfo.loggedIn !== nextProps.userInfo.loggedIn) && nextProps.userInfo.loggedIn) {
             await this.renderPopularProducts(nextProps.userInfo.token)
         }
+    }
+
+    async getRank() {
+        // TODO get route dynamically
+        const response = await fetch('/api/Personalizer/rank');
+        const data = await response.json();
+        console.log();
+        let newRanking = this.updateProducts(data);
+        this.setState({ recommendedProducts: newRanking });
+    }
+
+    updateProducts(data) {
+        var recommended = this.state.recommendedProducts;//[...this.state.recommendedProducts];
+        var tmp = recommended[0];
+        var curHeroIndex = recommended.findIndex(obj => obj.title === data.rewardActionId);
+        var hero = recommended[curHeroIndex];
+        recommended[curHeroIndex].title = tmp.title;
+        recommended[curHeroIndex].imageUrl = tmp.imageUrl;
+        recommended[curHeroIndex].url = tmp.url;
+        recommended[0].title = hero.title;
+        recommended[0].imageUrl = hero.imageUrl;
+        recommended[0].url = hero.url;
+
+        //var tmp = recommendedProductClone[recommendedProductsClone.findIndex(obj => obj.title === data.rewardActionId)];
+        //recommendedProductClone[0] = 
+        ////var recommendedProducts = this.state.recommendedProducts;
+        //for (i = 0; i < ranking.length; i++) {
+        //    var index = recommendedProductsClone.findIndex(obj => obj.title === ranking[i].id);
+
+        //    recommended[i].title = recommendedProductsClone[index].title;
+        //    recommended[i].imageUrl = recommendedProductsClone[index].imageUrl;
+        //    recommended[i].url = recommendedProductsClone[index].url;
+        //}
+        return recommended;
     }
 
     async renderPopularProducts(token) {
---
+++ Source/Tailwind.Traders.Web/Controllers/PersonalizerController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.CognitiveServices.Personalizer;
+using Microsoft.Azure.CognitiveServices.Personalizer.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Tailwind.Traders.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonalizerController : Controller
+    {
+        private PersonalizerClient _personalizerClient;
+        private static readonly string ApiKey = Environment.GetEnvironmentVariable("PERSONALIZER_RESOURCE_KEY");
+        private static readonly string ServiceEndpoint = Environment.GetEnvironmentVariable("PERSONALIZER_RESOURCE_ENDPOINT");
+
+        public PersonalizerController()
+        {
+            _personalizerClient = GetPersonalizerClient(ServiceEndpoint);
+        }
+
+        [HttpGet("Rank")]
+        public IActionResult Get()
+        {
+            RankRequest request = GetRankRequest();
+            RankResponse response = _personalizerClient.Rank(request);
+            return Ok(response);
+        }
+
+        private PersonalizerClient GetPersonalizerClient(string url)
+        {
+            PersonalizerClient client;
+            try
+            {
+                var credentials = new ApiKeyServiceClientCredentials(ApiKey);
+                client = new PersonalizerClient(new ApiKeyServiceClientCredentials(ApiKey))
+                {
+                    Endpoint = url
+                };
+            }
+            catch
+            {
+                client = null;
+            }
+            return client;
+        }
+
+        private RankRequest GetRankRequest()
+        {
+            string timeOfDay = DateTime.Now.Hour.ToString();
+            string dayOfWeek = DateTime.Now.DayOfWeek.ToString();
+            string userAgent = Request.Headers["User-Agent"].ToString();
+            string osInfo = userAgent.Substring(userAgent.IndexOf("(") + 1, userAgent.IndexOf(")"));
+
+            IList<object> currentContext = new List<object>()
+            {
+                new { time = timeOfDay },
+                new { weekday = dayOfWeek },
+                new { userOS = osInfo }
+            };
+
+            string eventId = Guid.NewGuid().ToString();
+
+            IList<object> gardenCenterFeatures = new List<object>()
+            {
+                new { avgPrice = 10 },
+                new { categorySize = 50 }
+            };
+
+            IList<object> powerToolsFeatures = new List<object>()
+            {
+                new { avgPrice = 20 },
+                new { categorySize = 100 },
+                new { childproof = false }
+            };
+            IList<object> plumbingFeatures = new List<object>()
+            {
+                new { avgPrice = 30 },
+                new { categorySize = 150 }
+            };
+            IList<object> electricalFeatures = new List<object>()
+            {
+                new { avgPrice = 40 },
+                new { categorySize = 200 }
+            };
+
+            IList<RankableAction> actions = new List<RankableAction>()
+            {
+                new RankableAction("Garden Center", gardenCenterFeatures),
+                new RankableAction("Power Tools", powerToolsFeatures),
+                new RankableAction("Electrical", electricalFeatures),
+                new RankableAction("Plumbing", plumbingFeatures),
+            };
+
+            return new RankRequest(actions, currentContext, eventId: eventId);
+        }
+    }
+}
