c3eceae912eb956da2f6a9b379629e5592a56ea1
Borja García <bgarcia@plainconcepts.com>
2021-05-21 09:17:22 +0200

    add acs as feature flag if no settings provided (#251)

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/ClientApp/src/pages/home/home.js
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/ClientApp/src/pages/home/home.js
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Hero, Recommended, Getapp, Popular } from "./components";
 import ChatBubble from './components/chatBubble/chatBubble';
+import { ConfigService } from './../../services'
 
 const Home = ({ recommendedProducts, popularProducts, loggedIn }) => {
+    const [customerSupportEnabled, setCustomerSupportEnabled] = useState(false);
+    useEffect(() => {
+        async function loadSettings() {
+            await ConfigService.loadSettings();
+            setCustomerSupportEnabled(ConfigService._customerSupportEnabled);
+        }
+        loadSettings();
+    },[])
     return (
         <div className="home">
             <Hero />
             <Recommended recommendedProductsData={recommendedProducts} loggedIn={loggedIn} />
             <Getapp />
-            {loggedIn && <Popular popularProductsData={popularProducts} />}
-            <ChatBubble />
+            {loggedIn && <Popular popularProductsData={popularProducts} />} 
+            { customerSupportEnabled && <ChatBubble />}
         </div>
     );
 };
